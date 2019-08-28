2375e3f40c274ffe5e8ef1002b68c34ed839c4f6
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-08-28 16:44:11 -0700

    began helper functions

--- checklist.py
+++ checklist.py
@@ -20,8 +20,22 @@ def destroy(index):
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print(str(index) + list_item)
+        print("{} {}".format(index, list_item))
         index += 1
+
+
+def select(function_code):
+
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+        read(item_index)
+    elif function_code == "P":
+        list_all_items()
+    else:
+        print("Unknown Option")
 
 
 def test():
