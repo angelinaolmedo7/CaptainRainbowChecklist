a8a68f134473b823e180cfab3180d907a5cb76bc
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-08-28 16:11:21 -0700

    Initial commit

---
+++ checklist.py
@@ -0,0 +1,34 @@
+checklist = list()
+
+
+def create(item):
+    checklist.append(item)
+
+
+def read(index):
+    return checklist[index]
+
+
+def update(index, item):
+    checklist[index] = item
+
+
+def destroy(index):
+    checklist.pop(index)
+
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    print(read(1))
+
+
+test()
