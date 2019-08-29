5c779ff18eb2586cf30ef3daafc9825526da9e3e
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-08-29 13:59:35 -0700

    color coding
    items added to list will be color coded

--- checklist.py
+++ checklist.py
@@ -20,7 +20,7 @@ def destroy(index):
 def list_all_items():
     index = 0
     for list_item in checklist:
-        print("{} {}".format(index, list_item))
+        print("{} {}".format(index, color_code(list_item)))
         index += 1
 
 
@@ -31,26 +31,30 @@ def mark_completed(index):
 def select(function_code):
 
     if function_code.upper() == "C":
-        input_item = user_input("Input item:")
+        input_item = user_input("Input item: ")
         create(input_item)
     elif function_code.upper() == "HELP":
-        print("\nPress:\nC to add to list\nR to read from list\nP to display list\nD to remove an item by index\nRm to remove an item by name\nQ to quit\n")
+        print("\nPress:\nC to add to list\nR to read from list\nP to display list\nU to update an item by index\nD to remove an item by index\nRm to remove an item by name\nQ to quit\n")
     elif function_code.upper() == "R":
-        item_index = int(user_input("Index Number?"))
-        print(read(item_index))
+        item_index = int(user_input("Index number: "))
+        print(color_code(read(item_index)))
     elif function_code.upper() == "P":
         list_all_items()
+    elif function_code.upper() == "U":
+        input_index = int(user_input("Index number: "))
+        input_item = user_input("Input new item: ")
+        update(input_index,input_item)
     elif function_code.upper() == "Q":
         return False
     elif function_code.upper() == "D":
-        item_index = int(user_input("Index Number?"))
+        item_index = int(user_input("Index number: "))
         destroy(item_index)
     elif function_code.upper() == "RM":
-        input_item = user_input("Input item to remove:")
+        input_item = user_input("Input item to remove: ")
         if input_item in checklist:
             checklist.remove(input_item)
         else:
-            print("Item not found in list. Check spelling and capitalization\n")
+            print("Item not found in list. Check spelling and capitalization.\n")
     else:
         print("Unknown Option")
     return True
@@ -59,6 +63,26 @@ def select(function_code):
 def user_input(prompt):
     user_input = input(prompt)
     return user_input
+
+
+def color_code(item):
+    print_item = str(item).upper()
+    if "RED" in print_item:
+        return "\033[31m{}\033[00m".format(item)
+    elif "ORANGE" in print_item:
+        return "\033[91m{}\033[00m".format(item)
+    elif "YELLOW" in print_item:
+        return "\033[93m{}\033[00m".format(item)
+    elif "GREEN" in print_item:
+        return "\033[32m{}\033[00m".format(item)
+    elif "BLUE" in print_item:
+        return "\033[34m{}\033[00m".format(item)
+    elif "PURPLE" in print_item:
+        return "\033[35m{}\033[00m".format(item)
+    elif "RAINBOW" in print_item:
+        return "\033[35m{}\033[00m".format(item)
+    else:
+        return item
 
 
 def test():
